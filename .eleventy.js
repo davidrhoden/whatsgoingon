839da3bbd39488b23976227a5b07fc370643cd8e
David Rhoden <david@davidrhoden.com>
2026-08-13 09:35:19 -0500

    robots.txt: go 'way

--- .eleventy.js
+++ .eleventy.js
@@ -86,6 +86,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Don't process folders with static assets e.g. images
+  eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("admin");
